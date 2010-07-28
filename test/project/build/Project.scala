c5498214233bd3fa59e15aa067481e4966c3888b
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-07-28 17:45:44 +0200

    closes #20: Changed bndClasspath property; also changed some other properties and improved ScalaDoc.

--- test/project/build/Project.scala
+++ test/project/build/Project.scala
@@ -1,13 +1,17 @@
-import com.weiglewilczek.bnd4sbt.BNDPlugin
+import com.weiglewilczek.bnd4sbt._
 import sbt._
 
 class TestProject(info: ProjectInfo) extends ParentProject(info: ProjectInfo) {
 
   lazy val a = project("a", "a", new A(_))
+
   class A(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin {
-    import com.weiglewilczek.bnd4sbt.ExecutionEnvironments._
+    import ExecutionEnvironment._
+
+    lazy val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
+
     override def bndBundleLicense = Some("Eclipse Public License v1.0")
-    override def bndExportPackage = Seq("com.weiglewilczek.bnd4sbttest.a;version=1.0")
+    override def bndExportPackage = Seq("com.weiglewilczek.bnd4sbttest.a;version=\"1.0\"")
     override def bndExecutionEnvironment = Set(Java5, Java6)
     override def bndDynamicImportPackage = Seq("x.y.z")
     override def bndVersionPolicy = Some("[$(@),$(@)]")
@@ -19,17 +23,20 @@ class TestProject(info: ProjectInfo) extends ParentProject(info: ProjectInfo) {
   }
 
   lazy val b = project("b", "b", new B(_), a)
+
   class B(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin {
     lazy val osgiCore = "org.osgi" % "org.osgi.core" % "4.2.0" % "provided"
     override def bndBundleActivator = Some("com.weiglewilczek.bnd4sbttest.b.internal.Activator")
   }
 
   lazy val c = project("c", "c", new C(_))
+
   class C(info: ProjectInfo) extends DefaultProject(info) {
     lazy val commonsIO = "commons-io" % "commons-io" % "1.4"
   }
 
   lazy val d = project("d", "d", new D(_), c)
+
   class D(info: ProjectInfo) extends DefaultProject(info) with BNDPlugin {
     lazy val osgiCore = "org.osgi" % "org.osgi.core" % "4.2.0" % "provided"
     lazy val commonsLogging = "commons-logging" % "commons-logging-api" % "1.1"
