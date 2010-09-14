51b66c2d3882a4a6fb68453ef0dc2e396dd54804
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-09-14 10:56:21 +0200

    closes #23: Bumped project version up to 1.0.0 final.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -15,6 +15,8 @@ class Project(info: ProjectInfo) extends PluginProject(info) {
 
   // Publishing
   override def managedStyle = ManagedStyle.Maven
-  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  //  lazy val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+  //  lazy val publishTo = Resolver.file("Local Test Repository", Path fileProperty "java.io.tmpdir" asFile)
 }
