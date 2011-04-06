0294447c753ef407d15491439918a75c315cd690
Heiko Seeberger <heiko.seeberger@googlemail.com>
2011-04-06 09:19:22 +0200

    Release 1.0.2: Changed version to 1.0.2 and switched publishTo to .../releases.

--- project/build/Bnd4SbtProject.scala
+++ project/build/Bnd4SbtProject.scala
@@ -15,7 +15,7 @@ class Bnd4SbtProject(info: ProjectInfo) extends PluginProject(info) {
   // Publishing
   override def managedStyle = ManagedStyle.Maven
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
-//  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
 //  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
-  val publishTo = Resolver.file("Local Test Repository", Path fileProperty "java.io.tmpdir" asFile)
+//  val publishTo = Resolver.file("Local Test Repository", Path fileProperty "java.io.tmpdir" asFile)
 }
